3e04b39c0595ea54aa779b5c67c4a85cca2137d8
Alexandr <gigi95@mail.ru>
2026-03-01 22:58:01 +0300

    Сделал слайдер

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
-
   const mixer = mixitup('.directions_list');
 
   $('.directions_filter-btn').on('click', function (){
     $('.directions_filter-btn').removeClass('directions_filter-btn--active')
     $(this).addClass('directions_filter-btn--active')
-  }) 
+  })
+  
+  $('.team_slider').slick({
+    arrows: false,
+    slidesToShow:4,
+    infinite: true,
+    draggable: false,
+    waitForAnimate: false,
+  })
+
+
+  $('.team_slider-prev').on('click', function(e){
+  e.preventDefault()
+  $('.team_slider').slick('slickPrev')
+})
+
+$('.team_slider-next').on('click', function(e){
+  e.preventDefault()
+  $('.team_slider').slick('slickNext')
+})
+  
+
+  
